03459ea3e38cb22872a554d61d26491024b8c014
Sanyam4001 <112722208+Sanyam4001@users.noreply.github.com>
2023-12-26 14:13:54 +0530

    New UI

---
+++ lib/constants/font_family.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FontFamily {
+  final String name;
+
+  const FontFamily(this.name);
+
+  // You may define more font families as needed
+  static const FontFamily defaultFamily = FontFamily('Default');
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,29 @@
-import 'package:bukizz_1/constants/constants.dart';
-import 'package:bukizz_1/pages/Home_Screen2.dart';
-import 'package:bukizz_1/pages/main_login.1.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// main.dart
 
-import 'auth/firebase_auth.dart';
-import 'auth/user_details.dart';
+import 'package:bukizz_frontend/screens/Signin_Screen.dart';
+import 'package:flutter/material.dart';
+
+
+
+// Import the new screen
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
 
-  UserDetails? savedUser = await UserDetails.loadFromSharedPreferences();
-  AppConstants.userData = savedUser!;
-
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => AuthProvider(),
-      child: MaterialApp(
-        theme: ThemeData.dark().copyWith(
-          primaryColor: const Color(0xFF0A0E21),
-          scaffoldBackgroundColor: const Color(0xFF0A0E21),
-        ),
-        home: savedUser != null ? HomeScreen() : SignInScreen(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        primaryColor: const Color(0xFF0A0E21),
-        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+
       // Use BottomNavigationScreen as the home screen
-      home: ChangeNotifierProvider(
-        create: (context) => AuthProvider(),
-        child: SignInScreen(),
-      )
+      home:SignIn(),
     );
   }
 }
