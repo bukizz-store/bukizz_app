8420dd690b1707a1aaa5640a9d72a68bdb60a535
Sanyam4001 <112722208+Sanyam4001@users.noreply.github.com>
2024-02-05 15:09:07 +0530

    view all stationary fixed

--- lib/ui/screens/HomeView/Ecommerce/product/view_all_stationary.dart
+++ lib/ui/screens/HomeView/Ecommerce/product/view_all_stationary.dart
@@ -33,87 +33,89 @@ class _ViewAllSchoolScreenState extends State<ViewAllSchoolScreen> {
       appBar: AppBar(
         title: const Text('Buy stationary'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: dimensions.height8),
-          Padding(
-            padding:EdgeInsets.symmetric(horizontal: dimensions.width16),
-            child: CustomTextForm(
-              width: dimensions.width342,
-              height: dimensions.height8*6,
-              controller: formController,
-              hintText: 'Enter School bags,lunch box',
-              icon: Icons.search,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: dimensions.height8),
+            Padding(
+              padding:EdgeInsets.symmetric(horizontal: dimensions.width16),
+              child: CustomTextForm(
+                width: dimensions.width342,
+                height: dimensions.height8*6,
+                controller: formController,
+                hintText: 'Enter School bags,lunch box',
+                icon: Icons.search,
+              ),
             ),
-          ),
 
-          SizedBox(height: dimensions.height8*1.5),
+            SizedBox(height: dimensions.height8*1.5),
 
-          Expanded(
-            child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  // crossAxisSpacing: dimensions.width24/3,
-                  mainAxisSpacing: dimensions.height8,
-                ),
-                itemCount: 4,
-                itemBuilder: (context, index) {
-            
-                  return Container(
-                    margin: EdgeInsets.symmetric(horizontal: dimensions.width24,vertical: dimensions.height10/2),
-                    width: dimensions.width146,
-                    height: dimensions.height10,
-                    decoration: ShapeDecoration(
-                      color: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        side: const BorderSide(
-                          width: 0.50,
-                          strokeAlign: BorderSide.strokeAlignOutside,
-                          color: Color(0xFFD6D6D6),
+            Container(
+              width: dimensions.screenWidth,
+              height: dimensions.screenHeight,
+              child:  GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    // crossAxisSpacing: dimensions.width24/3,
+                    mainAxisSpacing: dimensions.height8,
+                  ),
+                  itemCount: 4,
+                  itemBuilder: (context, index) {
+
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: dimensions.width24/2,vertical: dimensions.height10/2),
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(
+                            width: 0.50,
+                            strokeAlign: BorderSide.strokeAlignOutside,
+                            color: Color(0xFFD6D6D6),
+                          ),
+                          borderRadius: BorderRadius.circular(12),
                         ),
-                        borderRadius: BorderRadius.circular(12),
+                        shadows: const [
+                          BoxShadow(
+                            color: Color(0x2600579E),
+                            blurRadius: 12,
+                            offset: Offset(0, 4),
+                            spreadRadius: 0,
+                          )
+                        ],
                       ),
-                      shadows: const [
-                        BoxShadow(
-                          color: Color(0x2600579E),
-                          blurRadius: 12,
-                          offset: Offset(0, 4),
-                          spreadRadius: 0,
-                        )
-                      ],
-                    ),
-            
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          width: dimensions.width146,
-                          height: dimensions.height10*9,
-                          child: ClipRRect(
-                              borderRadius:BorderRadius.only(topLeft: Radius.circular(12),topRight: Radius.circular(12)),
-                              child: Image.asset('assets/stationary/${index+1}.jpg',fit: BoxFit.cover,)
+
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: dimensions.width10*16.8,
+                            height: dimensions.height10*9.5,
+                            child: ClipRRect(
+                                borderRadius:BorderRadius.only(topLeft: Radius.circular(12),topRight: Radius.circular(12)),
+                                child: Image.asset('assets/stationary/${index+1}.jpg',fit: BoxFit.cover,)
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: dimensions.width24/2,vertical: dimensions.height10*2),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              ReusableText(text: stationaryText[index], fontSize: 14,color: Color(0xFF444444),fontWeight: FontWeight.w500,),
-                              SizedBox(height: dimensions.height10*2,),
-                              ReusableText(text: stationarySubText[index], fontSize: 14,color:  Color(0xFF121212),fontWeight: FontWeight.w700,),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  );
-                }
-            ),
-          )
-        ],
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: dimensions.width24/2,vertical: dimensions.height10*2),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ReusableText(text: stationaryText[index], fontSize: 14,color: Color(0xFF444444),fontWeight: FontWeight.w500,),
+                                SizedBox(height: dimensions.height10*2,),
+                                ReusableText(text: stationarySubText[index], fontSize: 14,color:  Color(0xFF121212),fontWeight: FontWeight.w700,),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }
+              ),
+            )
+          ],
+        ),
       ),
 
     );
